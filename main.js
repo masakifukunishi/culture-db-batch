cae91bf0d9b61a8a3fa39e5480518b9ab8934093
MasakiFukunishi <fmsknn@gmail.com>
2023-03-30 10:42:40 -0700

    Add get collection

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const openAIApi = require("./lib/openAIApi");
+const mongoDB = require("./lib/mongoDB");
 
 async function main() {
   // let countries = await openAIApi.getCountries();
-  let countries = ["UnitedKingdom"];
-  countries = await openAIApi.getCountriesInfo(countries);
+  // let countries = ["UnitedKingdom"];
+  // countries = await openAIApi.getCountriesInfo(countries);
+
+  console.log(await mongoDB.getCollection());
 }
 main();
